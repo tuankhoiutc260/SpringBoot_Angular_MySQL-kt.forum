47e33eddd1f67921f4ebcbc55f4ffab191bc1f60
Nguyễn Tuấn Khôi <66896019+tuankhoiutc260@users.noreply.github.com>
2024-09-18 15:18:32 +0700

    feat(backend): save token to cookie

--- backend/src/main/java/com/tuankhoi/backend/configuration/WebConfiguration.java
+++ backend/src/main/java/com/tuankhoi/backend/configuration/WebConfiguration.java
@@ -5,15 +5,18 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.annotation.Nonnull;
+
 @Configuration
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class WebConfiguration implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@Nonnull CorsRegistry registry) {
         WebMvcConfigurer.super.addCorsMappings(registry);
         registry.addMapping("/**")
                 .allowedHeaders("*")
                 .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowCredentials(true);
     }
 }
--- backend/src/main/java/com/tuankhoi/backend/controller/AuthenticationController.java
+++ backend/src/main/java/com/tuankhoi/backend/controller/AuthenticationController.java
@@ -11,9 +11,13 @@ import com.tuankhoi.backend.dto.response.IntrospectResponse;
 import com.tuankhoi.backend.service.AuthenticationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,12 +33,27 @@ import java.text.ParseException;
 public class AuthenticationController {
     AuthenticationService authenticationService;
 
+    @NonFinal
+    @Value("${cookie.max-age-remember-me-duration}")
+    private int COOKIE_MAX_AGE_REMEMBER_ME_DURATION;
+
+    @NonFinal
+    @Value("${cookie.max-age-default-duration}")
+    private int COOKIE_MAX_AGE_DEFAULT_DURATION;
+
+    @NonFinal
+    @Value("${jwt.valid-duration}")
+    protected int JWT_VALID_DURATION;
+
     @Operation(summary = "Login user", description = "Authenticate a user and return a token.")
     @PostMapping("/login")
-    public APIResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
-        var result = authenticationService.authenticate(request);
+    public APIResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest authenticationRequest, HttpServletResponse httpServletResponse) {
+        AuthenticationResponse authResponse = authenticationService.authenticate(authenticationRequest);
+
+        setTokenCookie(httpServletResponse, authResponse.getToken(), authenticationRequest.isRememberMe());
+
         return APIResponse.<AuthenticationResponse>builder()
-                .result(result)
+                .result(authResponse)
                 .build();
     }
 
@@ -42,17 +61,17 @@ public class AuthenticationController {
     @PostMapping("/introspect")
     public APIResponse<IntrospectResponse> authenticate(@RequestBody IntrospectRequest request)
             throws ParseException, JOSEException {
-        var result = authenticationService.introspect(request);
         return APIResponse.<IntrospectResponse>builder()
-                .result(result)
+                .result(authenticationService.introspect(request))
                 .build();
     }
 
     @Operation(summary = "Logout user", description = "Logout the authenticated user.")
     @PostMapping("/logout")
-    public APIResponse<Void> logout(@RequestBody LogoutRequest request)
+    public APIResponse<Void> logout(@RequestBody LogoutRequest logoutRequest, HttpServletResponse httpServletResponse)
             throws ParseException, JOSEException {
-        authenticationService.logout(request);
+        authenticationService.logout(logoutRequest);
+        clearTokenCookie(httpServletResponse);
         return APIResponse.<Void>builder()
                 .build();
     }
@@ -61,9 +80,26 @@ public class AuthenticationController {
     @PostMapping("/refresh-token")
     public APIResponse<AuthenticationResponse> refreshToken(@RequestBody RefreshRequest refreshRequest)
             throws ParseException, JOSEException {
-        var result = authenticationService.refreshToken(refreshRequest);
         return APIResponse.<AuthenticationResponse>builder()
-                .result(result)
+                .result(authenticationService.refreshToken(refreshRequest))
                 .build();
+    }
+
+    public void setTokenCookie(HttpServletResponse response, String token, boolean rememberMe) {
+        Cookie cookie = new Cookie("authToken", token);
+        cookie.setHttpOnly(true); // Không thể truy cập từ JavaScript
+        cookie.setSecure(true); // Cookie chỉ được gửi qua HTTPS
+        cookie.setPath("/"); // Đường dẫn cookie có hiệu lực
+        cookie.setMaxAge(rememberMe ? COOKIE_MAX_AGE_REMEMBER_ME_DURATION : COOKIE_MAX_AGE_DEFAULT_DURATION);
+        response.addCookie(cookie);
+    }
+
+    public void clearTokenCookie(HttpServletResponse response) {
+        Cookie cookie = new Cookie("authToken", null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
